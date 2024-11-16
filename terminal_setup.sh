eaff8a47e7a1bfa104c3aded585a6863a26cc56a
Arseny Nikitin <nikitin.arceny@gmail.com>
2024-11-17 00:06:11 +0100

    create terminal_setup.sh

---
+++ terminal_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo pacman --noconfirm -S zsh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+sed -i 's|plugins=(|plugins=(zsh-syntax-highlighting |' ~/.zshrc
+sed -i 's|ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc
+sudo sed -i 's|#Color|Color|' /etc/pacman.conf
+echo "if [ -f ~/.zsh_aliases ]; then\n    . ~/.zsh_aliases\nfi" >> ~/.zshrc
+echo "alias ip='ip -color=auto'" >> ~/.zsh_aliases
+echo "alias ls='ls --color=auto'" >> ~/.zsh_aliases
+echo "include /usr/share/nano/sh.nanorc" >> ~/.nanorc
+
+source ~/.zshrc

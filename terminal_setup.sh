1630a73621e3382f482954dd5260bfa033ced401
Mr. Arceni <nikitin.arceny@gmail.com>
2024-11-17 11:29:19 +0100

    fix terminal_setup.sh

--- terminal_setup.sh
+++ terminal_setup.sh
@@ -3,8 +3,8 @@
 sudo pacman --noconfirm -S zsh
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 
 sed -i 's|plugins=(|plugins=(zsh-syntax-highlighting |' ~/.zshrc
 sed -i 's|ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc

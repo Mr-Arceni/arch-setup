b50b79ab6612f1db4b1b4293f8497d959e1f6a93
Mr. Arceni <nikitin.arceny@gmail.com>
2024-11-17 14:15:24 +0100

    change exec order in terminal_setup.sh

--- terminal_setup.sh
+++ terminal_setup.sh
@@ -2,16 +2,16 @@
 
 sudo pacman --noconfirm -S zsh
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 
 sed -i 's|plugins=(|plugins=(zsh-syntax-highlighting |' ~/.zshrc
 sed -i 's|ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc
 sudo sed -i 's|#Color|Color|' /etc/pacman.conf
-echo "if [ -f ~/.zsh_aliases ]; then\n    . ~/.zsh_aliases\nfi" >> ~/.zshrc
+echo "if [ -f ~/.zsh_aliases ]; then . ~/.zsh_aliases fi" >> ~/.zshrc
 echo "alias ip='ip -color=auto'" >> ~/.zsh_aliases
 echo "alias ls='ls --color=auto'" >> ~/.zsh_aliases
 echo "include /usr/share/nano/sh.nanorc" >> ~/.nanorc
 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 source ~/.zshrc
